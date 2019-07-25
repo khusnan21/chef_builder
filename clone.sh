e271fdaffccc52d17fab0bc9c8790d2a3403c769
Rohan Hasabe <43720061+Hasaber8@users.noreply.github.com>
2019-07-26 00:58:24 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+git clone https://github.com/LineageOS/android_kernel_motorola_msm8998 kernel/motorola/chef
+
+git clone https://github.com/LineageOS/android_packages_resources_devicesettings packages/resources/devicesettings
+
+git clone https://github.com/Hasaber8/proprietary_vendor_motorola vendor/motorola
+
+git clone https://github.com/LineageOS/android_external_bson external/bson
+
+git clone https://github.com/LineageOS/android_system_qcom system/qcom
+
+git clone https://github.com/PixelExperience-Devices/device_motorola_sdm660-common -b pie device/motorola/sdm660-common
+
+
 # write commands if you want to clone some repos
