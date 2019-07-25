a1a80ff7e8a1ac5ccf203096f5c9bcbd799b9418
Hasaber8 <rohanhasabe8@gmail.com>
2019-07-25 23:34:14 +0530

    Time for chef

--- config.sh
+++ config.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-GITHUB_USER=Akianonymus
-GITHUB_EMAIL=anonymus.aki@gmail.com
+GITHUB_USER=Hasaber8
+GITHUB_EMAIL=rohanhasabe8@gmail.com
 
-KBUILD_BUILD_USER="Aki"
+KBUILD_BUILD_USER="HasabeR8"
 KBUILD_BUILD_HOST="A_DEAD_PLANET"
 
 export oem=motorola
-export device=harpia
+export device=chef
 
 ROM="PixelExperience"
 manifest_url="https://github.com/PixelExperience/manifest"
 export rom_vendor_name="aosp" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
 branch="pie"
 
-release_repo="Akianonymus/harpia_builds"
+release_repo="Hasaber8/chef_builds"
 
 timezone="Asia/Kolkata"
