3129b6c42ab801aff1343e05a2f0bb1c0c06b896
khusnan21 <khusnan21@gmail.com>
2019-07-28 10:58:07 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -17,3 +17,22 @@ branch="pie"
 release_repo="yzfvyacheslavsky/builds"
 
 timezone="Asia/Jakarta"
+#!/bin/bash
+
+GITHUB_USER=khusnan21
+GITHUB_EMAIL=khusnan21@gmail.com
+
+KBUILD_BUILD_USER="pzzzt"
+KBUILD_BUILD_HOST="Telegram.org"
+
+export oem=xiaomi
+export device=tissot
+
+ROM="Havoc"
+manifest_url="https://github.com/Havoc-OS/android_manifest"
+export rom_vendor_name="havoc" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
+branch="pie"
+
+release_repo="khusnan21/builds"
+
+timezone="Asia/Jakarta"
