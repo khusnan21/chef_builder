02176105aca0f88b00ffb2eca149fca8e0a7d929
khusnan21 <khusnan21@gmail.com>
2019-07-28 11:01:34 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-GITHUB_USER=yzfvyacheslavsky
-GITHUB_EMAIL=yzfvyacheslavsky@gmail.com
-
-KBUILD_BUILD_USER="yzfvyacheslavsky"
-KBUILD_BUILD_HOST="Telegram.org"
-
-export oem=xiaomi
-export device=kenzo
-
-ROM="RR"
-manifest_url="https://github.com/ResurrectionRemix/platform_manifest"
-export rom_vendor_name="rr" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
-branch="pie"
-
-release_repo="yzfvyacheslavsky/builds"
-
-timezone="Asia/Jakarta"
-#!/bin/bash
-
 GITHUB_USER=khusnan21
 GITHUB_EMAIL=khusnan21@gmail.com
 
