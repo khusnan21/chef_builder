2bc5ea672be1021d394ecdce62652d4e5c91ca56
khusnan21 <khusnan21@gmail.com>
2019-07-28 10:52:22 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-GITHUB_USER=yzfvyacheslavsky
-GITHUB_EMAIL=yzfvyacheslavsky@gmail.com
+GITHUB_USER=khusnan21
+GITHUB_EMAIL=khusnan21@gmail.com
 
-KBUILD_BUILD_USER="yzfvyacheslavsky"
+KBUILD_BUILD_USER="pzzzt"
 KBUILD_BUILD_HOST="Telegram.org"
 
 export oem=xiaomi
-export device=kenzo
+export device=tissot
 
-ROM="RR"
-manifest_url="https://github.com/ResurrectionRemix/platform_manifest"
-export rom_vendor_name="rr" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
+ROM="Havoc"
+manifest_url="https://github.com/Havoc-OS/android_manifest"
+export rom_vendor_name="havoc" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
 branch="pie"
 
-release_repo="yzfvyacheslavsky/builds"
+release_repo="khusnan21/builds"
 
 timezone="Asia/Jakarta"
